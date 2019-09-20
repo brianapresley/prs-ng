b0b15217ddff52707cd992b479a4deecb308a997
Briana Presley <presley.briana@gmail.com>
2019-09-20 08:38:45 -0400

    Updated product and vendor detail

--- src/app/feature/product/product-detail/product-detail.component.ts
+++ src/app/feature/product/product-detail/product-detail.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Product } from '@model/product.class';
+import { ProductService } from '@svc/product.service';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-product-detail',
@@ -6,10 +9,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product-detail.component.css']
 })
 export class ProductDetailComponent implements OnInit {
-
-  constructor() { }
+  product: Product = new Product();
+  title: string = 'Product-Detail';
+  constructor(private productSvc: ProductService,
+    private router: Router,
+    private route: ActivatedRoute) { }
 
   ngOnInit() {
+        // Get the id from the web request, get the associated product record
+        this.route.params.subscribe(parms => {
+          this.productSvc.get(parms.id).subscribe(resp => {
+            this.product = resp as Product;
+            console.log('product detail: '+this.product.id);
+          })
+        });
   }
+  remove() {
+    this.productSvc.delete(this.product.id).subscribe(resp => {
+      alert('Product '+this.product.name+' successfully deleted!')
+      this.router.navigateByUrl('/product/list');
+    },
+    
+    err => {
+      console.log('error deleting product');
+      console.log(err);
 
+    });
+  }
 }
--- src/app/feature/vendor/vendor-detail/vendor-detail.component.ts
+++ src/app/feature/vendor/vendor-detail/vendor-detail.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Vendor } from '@model/vendor.class';
+import { VendorService } from '@svc/vendor.service';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-vendor-detail',
@@ -6,10 +9,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./vendor-detail.component.css']
 })
 export class VendorDetailComponent implements OnInit {
-
-  constructor() { }
+  vendor: Vendor = new Vendor();
+  title: string = 'Vendor-Detail';
+  constructor(private vendorSvc: VendorService,
+    private router: Router,
+    private route: ActivatedRoute) { }
 
   ngOnInit() {
+   // Get the id from the web request, get the associated vendor record
+       this.route.params.subscribe(parms => {
+        this.vendorSvc.get(parms.id).subscribe(resp => {
+         this.vendor = resp as Vendor;
+           console.log('vendor detail: '+this.vendor.id);
+         })
+     });
+  }
+  remove() {
+    this.vendorSvc.delete(this.vendor.id).subscribe(resp => {
+      alert('Vendor '+this.vendor.name+' successfully deleted!')
+      this.router.navigateByUrl('/vendor/list');
+    },
+    
+    err => {
+      console.log('error deleting vendor');
+      console.log(err);
+
+    });
   }
 
 }
